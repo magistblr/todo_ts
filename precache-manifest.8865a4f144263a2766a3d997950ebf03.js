b237564c34c559809439a65ac0825d39f58b9890
magistblr <78726323+magistblr@users.noreply.github.com>
2021-08-11 21:24:28 +0300

    Updates

---
+++ precache-manifest.8865a4f144263a2766a3d997950ebf03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "730313314908fe075f94802790678502",
+    "url": "/todo_ts/index.html"
+  },
+  {
+    "revision": "a53121f082e6e7b12bf6",
+    "url": "/todo_ts/static/css/main.f01e22a9.chunk.css"
+  },
+  {
+    "revision": "90f56059210652a99de1",
+    "url": "/todo_ts/static/js/2.2ebb2490.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/todo_ts/static/js/2.2ebb2490.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a53121f082e6e7b12bf6",
+    "url": "/todo_ts/static/js/main.29078b41.chunk.js"
+  },
+  {
+    "revision": "569610e31c2192b97df3",
+    "url": "/todo_ts/static/js/runtime-main.bd8c4400.js"
+  }
+]);
