9437d24498ae65dcfbd8ef4b53c101dbf74af395
magistblr <78726323+magistblr@users.noreply.github.com>
2021-10-15 20:22:49 +0300

    refactor

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import './App.css'
+import s from './App.module.css'
 import { TodolistsList } from '../features/TodolistsList/TodolistsList'
 import { useDispatch, useSelector } from 'react-redux'
 import { AppRootStateType } from './store'
@@ -7,7 +7,6 @@ import { RequestStatusType } from './app-reducer'
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import LinearProgress from '@mui/material/LinearProgress';
@@ -28,6 +27,7 @@ function App({demo = false}: PropsType) {
     const dispatch = useDispatch()
     const status = useSelector<AppRootStateType, RequestStatusType>((state) => state.app.status)
     const isInitialized = useSelector<AppRootStateType, boolean>(state => state.app.isInitialized)
+    const isLoggedIn = useSelector<AppRootStateType, boolean>(state => state.auth.isLoggedIn)
 
     useEffect(() => {
         dispatch(initializeAppTC())
@@ -48,15 +48,12 @@ function App({demo = false}: PropsType) {
         <div className="App">
             <ErrorSnackbar/>
             <AppBar position="static">
-                <Toolbar>
+                <Toolbar className={s.toolbar}>
                     <IconButton edge="start" color="inherit" aria-label="menu">
                         <Menu/>
                     </IconButton>
-                    <Typography variant="h6">
-                        News
-                    </Typography>
-                    <Button color="inherit">Login</Button>
-                    <Button onClick={onLogout} color="inherit">Logout</Button>
+                    {isLoggedIn && <Button className={s.online} color="inherit">Online</Button>}
+                    {isLoggedIn && <Button onClick={onLogout} color="inherit">Logout</Button>}
                 </Toolbar>
                 {status === 'loading' && <LinearProgress/>}
             </AppBar>
