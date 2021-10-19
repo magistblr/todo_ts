492dae4d7ed94e4f7f55cae0a38011259ce10350
magistblr <78726323+magistblr@users.noreply.github.com>
2021-10-19 09:42:56 +0300

    refactor

--- src/index.tsx
+++ src/index.tsx
@@ -8,17 +8,12 @@ import {store} from './app/store';
 import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
-    <React.StrictMode>
         <HashRouter>
             <Provider store={store}>
                 <App/>
             </Provider>
-        </HashRouter>
-    </React.StrictMode>,
+        </HashRouter>,
     document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
